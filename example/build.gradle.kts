ab87c3d898fa74acf06935ac1c978dc66e0e269c
Kirill Shashov <envoy93@gmail.com>
2021-05-21 22:11:34 +0400

    Extracted examples (#1)

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	id("war")
+	id("org.gretty") version "3.0.4"
 	id("com.vaadin") version "0.14.6.0"
 	kotlin("jvm") version "1.4.32"
 }
@@ -16,19 +18,27 @@ configurations {
 }
 
 repositories {
+	jcenter()
 	mavenCentral()
 }
+
+gretty {
+	contextPath = "/"
+	servletContainer = "jetty9.4"
+}
+
+extra["vaadinVersion"] = "14.5.3"
 
 dependencies {
 	implementation(enforcedPlatform("com.vaadin:vaadin-bom:14.6.0"))
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("io.reactivex.rxjava3:rxjava:3.0.12")
-	implementation("com.vaadin:vaadin-core")
 
 	compileOnly("org.projectlombok:lombok:1.18.20")
 	annotationProcessor("org.projectlombok:lombok")
+
+	compileOnly("javax.servlet:javax.servlet-api:3.1.0")
 }
 
 tasks.withType<KotlinCompile> {
